a301594a88369cdc14ce7907010d313f599adc42
MatteoRay <Krac1x@gmail.com>
2022-10-25 17:40:23 +0300

    message

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,13 +7,10 @@ const Header = () => {
     const {user, onClose} = useTelegram();
 
     return (
-        <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
+        <div className = {'header'}>
+           
         </div>
-    );
-};
+    )
+}
 
 export default Header;
